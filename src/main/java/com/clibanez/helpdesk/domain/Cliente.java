5a1578d4a56d78adc220e2880717e0a3c4e1e487
Clibanez <clibanezzcaldas@gmail.com>
2022-01-23 22:59:47 -0300

    CRUD para Cliente implementado.

--- src/main/java/com/clibanez/helpdesk/domain/Cliente.java
+++ src/main/java/com/clibanez/helpdesk/domain/Cliente.java
@@ -2,10 +2,12 @@ package com.clibanez.helpdesk.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
+import com.clibanez.helpdesk.domain.dtos.ClienteDTO;
 import com.clibanez.helpdesk.domain.enums.Perfil;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -16,6 +18,17 @@ public class Cliente extends Pessoa{
 	@JsonIgnore
 	@OneToMany(mappedBy = "cliente")
 	private List<Chamado> chamados = new ArrayList<>();
+	
+	public Cliente(ClienteDTO obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.cpf = obj.getCpf();
+		this.email = obj.getEmail();
+		this.senha = obj.getSenha();
+		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao = obj.getDataCriacao();
+	}
 
 	public Cliente() {
 		super();
