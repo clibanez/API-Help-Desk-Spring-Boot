6a39c48802c908105ea80860093da84e53f9f3cf
Clibanez <clibanezzcaldas@gmail.com>
2022-01-25 08:58:00 -0300

    endpoint findAll para chamado.

--- src/main/java/com/clibanez/helpdesk/services/ChamadoService.java
+++ src/main/java/com/clibanez/helpdesk/services/ChamadoService.java
@@ -1,5 +1,6 @@
 package com.clibanez.helpdesk.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,5 +20,11 @@ public class ChamadoService {
 		Optional <Chamado> obj = chamadoRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID: " + id));
 	}
+	
+	 public List<Chamado> findAll(){
+	        return chamadoRepository.findAll();
+	    }
+	
+	
 
 }
